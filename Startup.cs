c64268db0ae9bfc2beae70588fac68ef38c62de9
Rohan Ankarigari <23293949+Rahtoken@users.noreply.github.com>
2019-08-20 19:15:59 +0530

    Change root namespace to ZetsubouGacha.

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace zetsubou_gacha
+namespace ZetsubouGacha
 {
     public class Startup
     {
