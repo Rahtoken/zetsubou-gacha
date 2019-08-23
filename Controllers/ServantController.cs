d884e8b98e4f9c9afdfc8b3efdf3cdcd879dddfd
Rohan Ankarigari <23293949+Rahtoken@users.noreply.github.com>
2019-08-23 18:29:32 +0530

    Add ServantController. Enable connection with MongoDB database.

---
+++ Controllers/ServantController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZetsubouGacha.Models;
+using ZetsubouGacha.Services;
+
+namespace ZetsubouGacha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServantController: ControllerBase
+    {
+        private readonly ServantService _servantService;
+
+        public ServantController(ServantService servantService)
+        {
+            _servantService = servantService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Servant> Servants()
+        {
+            return _servantService.Get();
+        }
+    }
+}
---
+++ Services/ServantService.cs
@@ -0,0 +1,22 @@
+﻿using ZetsubouGacha.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System;
+
+namespace ZetsubouGacha.Services
+{
+    public class ServantService
+    {
+        private readonly IMongoCollection<Servant> _servants;
+
+        public ServantService(IZetsubouGachaDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _servants = database.GetCollection<Servant>(settings.ServantsCollectionName);
+        }
+
+        public List<Servant> Get() => _servants.Find(servant => true).ToList();
+    }
+}
