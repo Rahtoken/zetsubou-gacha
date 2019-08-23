f4cf50f221f9014bdd810a946b9c1a46baf994fb
Rohan Ankarigari <23293949+Rahtoken@users.noreply.github.com>
2019-08-23 18:28:54 +0530

    Add Servant data model.

--- Models/Servant.cs
+++ Models/Servant.cs
@@ -5,10 +5,22 @@ namespace ZetsubouGacha.Models
 {
     public class Servant
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
         public string Name { get; set; }
-        public string Image { get; set; }
+
+        public string Title { get; set; }
+
+        public string FirstAscensionImage { get; set; }
+
+        public string FinalAscensionImage { get; set; }
+
         public string Dialogue { get; set; }
+
         public string Audio { get; set; }
+
         public string Summary { get; set; }
     }
 }
