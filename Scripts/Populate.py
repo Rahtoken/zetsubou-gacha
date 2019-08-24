5c9501b7b36c987a4132aff92d543a1e6383f7e7
Rohan Ankarigari <23293949+Rahtoken@users.noreply.github.com>
2019-08-24 11:35:51 +0530

    Add utility script to populate database.

---
+++ Scripts/Populate.py
@@ -0,0 +1,18 @@
+# Run once, results in data duplication otherwise.
+from pymongo import MongoClient
+import os
+import json
+
+# Establishing database connection.
+client = MongoClient()
+db = client['ZetsubouGacha']
+servants_collection = db['Servants']
+
+# Loading the JSON data.
+data_path = r'..\TestData\servants.json'
+servant_data = []
+with open(os.path.join(os.path.dirname(__file__), data_path), 'r') as data:
+    servant_data = json.load(data)
+
+# Pushing to the database.
+servants_collection.insert_many(servant_data)
