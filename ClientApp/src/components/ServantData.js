4ed1018076c8a626c830cb747babb368a539a1d4
Akhil Anil M <47663052+zetsubou24@users.noreply.github.com>
2019-08-26 21:41:17 +0530

    Frontend Update (#10)
    * Frontend Update
    
    * PR changes
    
    * PR changes
    
    * Update servantDataUpdate2.json

---
+++ ClientApp/src/components/ServantData.js
@@ -0,0 +1,12 @@
+class ServantData{
+    constructor(data){
+        this.id=data.id
+        this.name=data.name
+        this.title=data.title
+        this.firstAscensionImage=data.firstAscensionImage
+        this.finalAscensionImage=data.finalAscensionImage
+        this.dialogue=data.dialogue
+        this.audio=data.audio
+    }
+}
+export default ServantData
