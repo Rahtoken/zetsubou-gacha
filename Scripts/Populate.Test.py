d645501b74025ff576457bd4ca39874de13b54c3
Restyled.io <commits@restyled.io>
2019-12-16 17:16:56 +0000

    Restyled by black

--- Scripts/Populate.Test.py
+++ Scripts/Populate.Test.py
@@ -5,13 +5,13 @@ import json
 
 # Establishing database connection.
 client = MongoClient()
-db = client['ZetsubouTests']
-servants_collection = db['Servants']
+db = client["ZetsubouTests"]
+servants_collection = db["Servants"]
 
 # Loading the JSON data.
-data_path = './TestData.json'
+data_path = "./TestData.json"
 servant_data = []
-with open(os.path.join(os.path.dirname(__file__), data_path), 'r') as data:
+with open(os.path.join(os.path.dirname(__file__), data_path), "r") as data:
     servant_data = json.load(data)
 
 # Dropping existing data
