19724baa4716d5941cd1ca7302b9c3f4190de7ef
Restyled.io <commits@restyled.io>
2019-12-16 17:16:57 +0000

    Restyled by clang-format

--- Test/Backend.Tests/ServantServiceTests.cs
+++ Test/Backend.Tests/ServantServiceTests.cs
@@ -3,39 +3,31 @@ using System.Linq;
 using ZetsubouGacha.Services;
 using ZetsubouGacha.Settings;
 
-namespace Backend.Tests
-{
-[TestClass]
-public class ServantServiceTests
-{
+namespace Backend.Tests {
+  [TestClass]
+  public class ServantServiceTests {
     [TestMethod]
-    public void AllServantsTest()
-    {
-        var settings = new DatabaseSettings()
-        {
-            ConnectionString = "mongodb://localhost:27017",
-            Database = "ZetsubouTests",
-            ServantsCollection = "Servants"
-        };
-        var servantService = new ServantService(settings);
-        int limit = 69;
-        var results = servantService.GetAllServantsAsync(limit).Result.ToList();
-        Assert.AreEqual(results.Count, limit);
+    public void
+    AllServantsTest() {
+      var settings = new DatabaseSettings(){
+          ConnectionString = "mongodb://localhost:27017",
+          Database = "ZetsubouTests", ServantsCollection = "Servants"};
+      var servantService = new ServantService(settings);
+      int limit = 69;
+      var results = servantService.GetAllServantsAsync(limit).Result.ToList();
+      Assert.AreEqual(results.Count, limit);
     }
 
     [TestMethod]
-    public void ServantByIdTest()
-    {
-        int id = 69;
-        var settings = new DatabaseSettings()
-        {
-            ConnectionString = "mongodb://localhost:27017",
-            Database = "ZetsubouTests",
-            ServantsCollection = "Servants"
-        };
-        var servantService = new ServantService(settings);
-        var result = servantService.GetServantByIdAsync(id).Result;
-        Assert.AreEqual(result.Id, 69);
+    public void
+    ServantByIdTest() {
+      int id = 69;
+      var settings = new DatabaseSettings(){
+          ConnectionString = "mongodb://localhost:27017",
+          Database = "ZetsubouTests", ServantsCollection = "Servants"};
+      var servantService = new ServantService(settings);
+      var result = servantService.GetServantByIdAsync(id).Result;
+      Assert.AreEqual(result.Id, 69);
     }
-}
+  }
 }
