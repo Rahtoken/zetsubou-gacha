9df76c38035e8c34e3c986547830f7fad671abc6
Zetsubou24 <alphabetaomega4@gmail.com>
2020-01-15 17:38:01 +0300

    Added Flip Animation

--- client/src/components/Servant.jsx
+++ client/src/components/Servant.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Card, Image, Button } from 'semantic-ui-react';
-
-const Servant = (props) => {
-
+import { Card, Image, Button, Transition } from "semantic-ui-react";
+const Servant = props => {
   const [image, changeImage] = useState(0);
-
+  const [visible, changeVisibilty] = useState(true);
   const images = [
     props.data.firstAscensionImage,
     props.data.finalAscensionImage
@@ -13,14 +11,40 @@ const Servant = (props) => {
   return (
     <>
       <Card raised>
-        <Image src={images[image]} wrapped ui={false} />
+        <Transition
+          visible={visible}
+          animation="horizontal flip"
+          duration={200}
+        >
+          {image % 2 === 0 ? (
+            <Image src={images[image]} wrapped ui={false} />
+          ) : (
+            <></>
+          )}
+        </Transition>
+
+        <Transition
+          visible={!visible}
+          animation="horizontal flip"
+          duration={200}
+        >
+          {image % 2 ? <Image src={images[image]} wrapped ui={false} /> : <></>}
+        </Transition>
+
         <Card.Content>
           <Card.Header>{props.data.name}</Card.Header>
           <Card.Meta>{props.data.title}</Card.Meta>
           <Card.Description>{props.data.dialogue}</Card.Description>
         </Card.Content>
       </Card>
-      <Button onClick={() => changeImage((image + 1) % 2)}>Transform!</Button>
+      <Button
+        onClick={() => {
+          changeImage((image + 1) % 2);
+          changeVisibilty(visible ^ true);
+        }}
+      >
+        Transform!
+      </Button>
     </>
   );
 };
